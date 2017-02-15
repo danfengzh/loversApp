8860f190908b87d9ddb371d70fca6d24efb7de8d
张丹峰 <3310816449@qq.com>
2017-02-15 09:45:06 +0800

    this  is third commit,修正了实体类的规范，实现了serieable接口，方便后续序列化操作

---
+++ src/main/java/org/loversAPP/model/DigHistory.java
@@ -0,0 +1,35 @@
+package org.loversAPP.model;
+
+import org.loversAPP.base.BaseEntity;
+
+public class DigHistory extends BaseEntity {
+    private Integer id;
+
+    private Integer userid;
+
+    private Integer treasureid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public Integer getTreasureid() {
+        return treasureid;
+    }
+
+    public void setTreasureid(Integer treasureid) {
+        this.treasureid = treasureid;
+    }
+}
---
+++ src/main/java/org/loversAPP/model/UserItem.java
@@ -0,0 +1,25 @@
+package org.loversAPP.model;
+
+import org.loversAPP.base.BaseEntity;
+
+public class UserItem extends BaseEntity {
+    private Integer userId;
+
+    private Integer itemId;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+}
